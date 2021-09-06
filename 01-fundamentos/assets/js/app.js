2761b29a6ae3f434addecef3802b4a53ce504f29
fremi <microsys.net@gmail.com>
2021-09-06 00:12:50 -0500

    JS

---
+++ 01-fundamentos/assets/js/app.js
@@ -0,0 +1,21 @@
+//console.log("Hola Mundo")
+
+alert("Hola desde app.js");
+
+let a = 10,
+  b = 20,
+  c = "hola ",
+  d = "spider",
+  x = a + b;
+
+const saludo = c + d;
+
+console.log("%c Mis Variables", "color: blue; font-weight: bold");
+console.log({ a });
+console.log({ b });
+console.log({ c });
+console.table({ a, b, c, d, x }); // table para crear una tabla
+
+c = "Hola de nuevo";
+
+var miNombre = "Freddy";
